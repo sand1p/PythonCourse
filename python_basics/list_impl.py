c74acd04be51a3261286b25df7f61a7a5c860467
Sandip Gatkal <sandip@ridecell.com>
2020-09-22 20:59:18 +0530

    built in functions for list

--- python_basics/list_impl.py
+++ python_basics/list_impl.py
@@ -1,3 +1,14 @@
-student_grades = ['A', 'B', 'C', 'D']
+# list declaration
+student_grades: [str] = [7.8, 9.2, 10, 8.7]
 
 print(student_grades)
+
+
+# Method: It is associated with object
+# Function: It is not associated with any object
+
+sum = sum(student_grades)
+length = len(student_grades)
+
+avg = sum / length
+print("average", avg)
--- python_basics/types.py
+++ python_basics/types.py
@@ -9,3 +9,12 @@ print(z + z)
 print(w + w)
 
 print("print types", type(x), type(y), type(z), type(w))
+
+
+# Getting data types and help
+# will povide all the methods on the int
+print(dir(int))
+upper = "sandip".upper()
+print(upper)
+
+print("title".title())
